fc927332ba3fc8dbe190e864b362d943892a1131
ewertonP <ewerton.paulo@dce.ufpb.br>
2019-04-25 19:28:54 -0300

    second

--- pln.py
+++ pln.py
@@ -15,49 +15,53 @@ shakespeare = load_text("shakespeare.txt")
 regex = "[a-zA-Z]+"
 tokens = re.findall(regex, shakespeare)
 
+#calculate the probability
 def w_ngran(word, sequence):
-    regex = ""
-    for i in range(len(sequence)):
-        if i == 0:
-            regex += sequence[i]
-        else:
-            regex += " "+sequence[i]
-    
+    regex = sequence
     count_total = shakespeare.count(regex+" "+word)
     count = shakespeare.count(regex)
     return count_total/count
 
+#sort by value from dictionary
 def sorted_probs(probs):
     return sorted(probs.items(), key=itemgetter(1), reverse=True)
 
-def word_after(sequence):
+#get all words after a entry of words with max lenght == 2 
+def word_after(sequence, _range):
     words = []
     lenght = len(tokens)-1
-    if len(sequence) == 1:
+    if _range == '2' or len(sequence) == 1:
         for i in range(lenght):
-            if tokens[i] == sequence[0]:
+            if tokens[i] == sequence[-1]:
                 words.append(tokens[i+1])
+        sequence = sequence[-1]
 
-    if len(sequence) == 2:
+    if _range == '3':
         for i in range(lenght):
-            if i < lenght-1 and tokens[i] == sequence[0] and tokens[i+1] == sequence[1]:
+            if i < lenght-2 and tokens[i] == sequence[-2] and tokens[i+1] == sequence[-1]:
                 words.append(tokens[i+2])
-
-    return words
+        sequence = " ".join([sequence[-2], sequence[-1]])
+    return (sequence, words)
 
 def most_likely():
+    print("x para sair!")
+    _range = input("type 2 for bigran, 3 for trigran: ")
     while True:
-        print("x para sair!")
-        entry = input("digite sua entrada: ")
+        entry = input("type: ")
         if entry == 'x':
             break
         sequence = entry.split()
-        after = word_after(sequence)
+        words, after = word_after(sequence, _range)
         probs = {}
         for i in after:
-            probs[i] = w_ngran(i, sequence)
-
-        print(sorted_probs(probs)[:3])
+            probs[i] = w_ngran(i, words)
+        
+        result = sorted_probs(probs)[:3]
+        if result:
+            for word, probability in result:
+                print("palavra: %s | probabilidade: %f" %(word,probability))
+        else:
+            print("No results avaliable")            
 
 most_likely()
     
